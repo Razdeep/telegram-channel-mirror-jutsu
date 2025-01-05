d780fb36ca4227a41105284cf16581bc30f7769f
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2025-01-05 16:23:30 +0530

    feat: added forward service code
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- config_example.py
+++ config_example.py
@@ -2,3 +2,5 @@ api_id = "PLACEHOLDER"
 api_hash = "PLACEHOLDER"
 channel_id_source = -100466554652  # 12 digit negative number, replace this also
 channel_id_destination = -100466554652  # 12 digit negative number, replace this also
+forward_channel_id_source=-1
+forward_channel_id_destination=-1
---
+++ forward_service.py
@@ -0,0 +1,24 @@
+from telethon import TelegramClient
+import config
+import logging
+import asyncio
+
+# Replace these with your API credentials
+api_id = config.api_id
+api_hash = config.api_hash
+
+
+async def forward_existing_messages():
+    try:
+        async with TelegramClient("download_session", api_id, api_hash) as client:
+            async for message in client.iter_messages(config.forward_channel_id_source):
+                await client.send_message(
+                    config.forward_channel_id_destination, message
+                )
+                logging.info(f"Message forwarded: {message.text}")
+    except Exception as e:
+        logging.exception(f"Failed to forward messages: {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(forward_existing_messages())
