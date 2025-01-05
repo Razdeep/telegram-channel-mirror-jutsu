693a5980c9a572a817f91ffd7d42bbb94de03d72
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2025-01-05 16:25:06 +0530

    fix: reversed iterator added
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- forward_service.py
+++ forward_service.py
@@ -11,7 +11,9 @@ api_hash = config.api_hash
 async def forward_existing_messages():
     try:
         async with TelegramClient("download_session", api_id, api_hash) as client:
-            async for message in client.iter_messages(config.forward_channel_id_source):
+            async for message in client.iter_messages(
+                config.forward_channel_id_source, limit=4000, reversed=True
+            ):
                 await client.send_message(
                     config.forward_channel_id_destination, message
                 )
