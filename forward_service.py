d7e4a69eff415ee57cabddd3d19087391cfd5d94
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2025-01-08 11:15:36 +0530

    feat: improved forward_service.py
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- forward_service.py
+++ forward_service.py
@@ -2,6 +2,8 @@ from telethon import TelegramClient
 import config
 import logging
 import asyncio
+import time
+import sys
 
 # Replace these with your API credentials
 api_id = config.api_id
@@ -12,15 +14,27 @@ async def forward_existing_messages():
     try:
         async with TelegramClient("download_session", api_id, api_hash) as client:
             async for message in client.iter_messages(
-                config.forward_channel_id_source, limit=4000, reversed=True
+                config.forward_channel_id_source, limit=4000, reverse=True
             ):
+                if not message.video:
+                    logging.info(
+                        f"skipping message id {message.id}, because it is not video"
+                    )
+                    continue
                 await client.send_message(
                     config.forward_channel_id_destination, message
                 )
                 logging.info(f"Message forwarded: {message.text}")
+                time.sleep(0.5)
     except Exception as e:
         logging.exception(f"Failed to forward messages: {e}")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(asctime)s]: %(message)s",
+        datefmt="%d/%b/%Y %H:%M:%S",
+        stream=sys.stdout,
+    )
     asyncio.run(forward_existing_messages())
